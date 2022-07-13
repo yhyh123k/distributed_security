c91b6194821cdee0068eb66dea5b8f83ac1f6bf7
sshyi <9530845+sshyi@user.noreply.gitee.com>
2022-07-13 14:58:22 +0800

    初始化父工程依赖

---
+++ src/main/java/com/example/distributed/security/DistributedSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributed.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedSecurityApplication.class, args);
+    }
+
+}
